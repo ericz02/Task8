b7ab86798b12edc01341dd2374dd4944ce208b24
ericz02 <ezheng0720@gmail.com>
2023-04-25 16:50:36 -0400

    imports all image into non-social links

--- components/NonSocialLinks.jsx
+++ components/NonSocialLinks.jsx
@@ -1,8 +1,40 @@
+import Image from "next/image"
+import snapchat from "../images/zip/snapchat.svg"
+import facebook from "../images/zip/facebook.svg"
+import instagram from "../images/zip/instagram.svg"
+import twitter from "../images/zip/twitter.svg"
+
 const NonSocialLinks = () => {
 
     return (
-        <div>
-            non social links
+        <div className="flex justify-center">
+
+        <Image
+            className=""
+            src={snapchat}
+            width={46}
+            height={43}
+        />
+
+        <Image 
+            src={facebook} 
+            width={46} 
+            height={46}
+        />
+
+        <Image 
+            src={instagram} 
+            width={46} 
+            height={46}
+        />
+
+        <Image 
+            src={twitter} 
+            width={46} 
+            height={38}
+        />
+
+
         </div>
     )
 }
